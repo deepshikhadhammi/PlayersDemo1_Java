2735ec5996ca72e4209b6130c8c8545fe39fb38f
Namit Aneja <namit.aneja@ucalgary.ca>
2022-02-24 23:37:18 -0700

    commit-2

--- src/project.java
+++ src/project.java
@@ -21,4 +21,31 @@ public class project {
         }
         return team;
     }
+
+    public static void menu() {
+        System.out.println(" Options Available to User");
+        System.out.println("Tracking details for a cricket Team ");
+        System.out.println(" Add a cricket team and Add player’s name");
+        System.out.println("Press 1 to Add  age, experience, position ,weight");
+        System.out.println("Press 2 to Add number of 6's by a player");
+        System.out.println("Press 3 to Add number of wickets by a player");
+        System.out.println("Press 4 to Add number of catches taken by a player");
+        System.out.println("Press 5 to find top  run scorers");
+        System.out.println("Press 6 Number of man of the match awards");
+        System.out.println("Press 7 Fitness- hours of practice for each player");
+        System.out.println("Press 8 Number of centuries by a player");
+        System.out.println("Press 9 Number of 5 wickets haul by a player");
+    }
+
+    public static HashMap<String, Integer> addPlayersAge(ArrayList name) {
+        HashMap<String, Integer> map = new HashMap<String, Integer>();
+        for (Object player : name) {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Enter " + player + " age");
+            int age = sc.nextInt();
+            map.put((String) player, age);
+        }
+        return map;
+
+    }
 }
