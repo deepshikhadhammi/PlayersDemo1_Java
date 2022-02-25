535b44480964eb31b777ff22ca0c31707427f824
Namit Aneja <namit.aneja@ucalgary.ca>
2022-02-24 23:30:52 -0700

    commit-1

--- src/project.java
+++ src/project.java
@@ -1,6 +1,24 @@
-public class project {
-    public static void main(String[] args) {
+import java.util.*;
 
-        System.out.println("namit");
+public class project {
+
+    public static ArrayList createTeam() {
+        ArrayList team;
+        team = new ArrayList();
+        return team;
+    }
+
+    public static ArrayList addPlayersName(ArrayList team) {
+        System.out.println("There are 11 players in a cricket Team");
+        int i = 1;
+        while (i < 12) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Enter Players Name");
+            String name = scanner.nextLine();
+            team.add(name);
+            i++;
+
+        }
+        return team;
     }
 }
